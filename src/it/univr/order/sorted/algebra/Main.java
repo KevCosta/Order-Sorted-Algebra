7982b5db2f81d9b37d275cc275e4d1d0c69fb76e
GiovanniBellorio <gbellorio95@gmail.com>
2018-10-05 20:56:15 +0200

    add Main

---
+++ src/it/univr/order/sorted/algebra/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package it.univr.order.sorted.algebra;
+
+/**
+ * @author giovanni
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
